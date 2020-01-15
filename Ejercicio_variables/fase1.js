fd2f30dbdfec92003d4f2a6b74807e34bb866ca4
Ivacks <ivan.navarro.vargas@gmail.com>
2020-01-15 18:52:37 +0100

    Modificación JS Fase 1

--- Ejercicio_variables/fase1.js
+++ Ejercicio_variables/fase1.js
@@ -5,11 +5,11 @@ var dia = 15;
 var mes = 01;
 var any = 2020;
 
-var totnom = cognom1 + " " + cognom2 + " " + nom;
+var totnom = cognom1 + " " + cognom2 + "," + nom;
 console.log(totnom);
 document.body.innerHTML = "<h1>" + cognom1 + " " + cognom2 + "," + nom + "</h1>";
 
 
 var data = dia + "/" + mes + "/" + any;
 console.log(data);
-document.body.innerHTML = "<h3>" + dia + "/" + mes + "/" + any + "</h3>";
+document.body.innerHTML = "<h1>" + dia + "/" + mes + "/" + any + "</h1>";
