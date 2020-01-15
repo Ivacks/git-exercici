1f0673f81a60740e7ec0e11223df75411c1e441a
Ivacks <ivan.navarro.vargas@gmail.com>
2020-01-15 11:09:43 +0100

    Ejercicio C# Fase 1

---
+++ Ejercicio_variables/Fase1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom = "Ivan";
+            string cognom1 = "Navarro";
+            string cognom2 = "Vargas";
+
+            int dia = 15;
+            int mes = 01;
+            int any = 2020;
+
+            Console.WriteLine($"{cognom1} {cognom2} {nom}");
+            Console.WriteLine($"{dia}/{mes}/{any}");
+
+
+        }
+    }
+}
