b151a9a7863ed94704c352fa2dbc86b5f5f3de4d
Ivacks <ivan.navarro.vargas@gmail.com>
2020-01-16 09:59:05 +0100

    Entrega fase 2 #

---
+++ Ejercicio_variables/Fase2/Fase2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fase2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int any_traspas = 1948;
+            Console.Write("Entra el teu any de naixement: ");
+            string enter_year = Console.ReadLine();
+            int my_year = Convert.ToInt32(enter_year);
+            int transition_year = 4;
+
+            int yearsoftransition = (my_year - any_traspas) / transition_year;
+
+            Console.WriteLine($"Del 1948 al teu naixement hi ha: {yearsoftransition} anys de traspàs.");
+
+            Console.ReadKey(true);
+
+
+            
+            
+
+        }
+    }
+}
