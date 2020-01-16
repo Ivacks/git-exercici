8bfcf8de846d899ecad5c7b0bafabe313cec6a0c
Ivacks <ivan.navarro.vargas@gmail.com>
2020-01-16 12:00:36 +0100

    Subida Fase 3 C#

---
+++ Ejercicio_variables/Fase3/Fase3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Fase3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year_one = 1948;
+            Console.Write("Entra el teu any de naixement: ");
+            string enter_year = Console.ReadLine();
+            int my_year = Convert.ToInt32(enter_year);
+            bool transition_year_check = false; 
+            
+
+
+            do
+            {
+                year_one++;
+              if (( (year_one % 4 == 0) && (year_one % 100 != 0)) || ((year_one % 100 == 0) && (year_one % 400 == 0)))
+                {
+                    if (year_one == my_year)
+                    {
+                        transition_year_check = true;
+                    }
+                    Console.Write($"El {year_one} és any de traspàs. \n");
+                }
+            } while (year_one <= my_year);
+            if (transition_year_check)
+            {
+                Console.Write($"El teu any de naixement {my_year} és de traspàs. \n");
+            } else
+            {
+                Console.Write($"El teu any de naixement {my_year} no és de traspàs. \n");
+            }
+            
+
+
+            Console.ReadKey(true);
+        }
+    }
+}
