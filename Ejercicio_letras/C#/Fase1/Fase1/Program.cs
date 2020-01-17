40d5ba55d1b75388cb8072f95a04eeeec6cf0f60
Ivacks <ivan.navarro.vargas@gmail.com>
2020-01-17 11:22:55 +0100

    Subida Fase 1 Letras C#

---
+++ Ejercicio_letras/C#/Fase1/Fase1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entra el teu nom: \n");
+            string nom = Console.ReadLine();
+            char[] desglossat_nom = nom.ToCharArray();
+            int i;
+            Console.Write("El teu nom desglossat és així: ");
+            for ( i = 0; i < desglossat_nom.Length ; i++)
+            { 
+                Console.Write(desglossat_nom[i] + " ");
+            }
+            Console.ReadKey(true);
+        }
+        
+    }
+}
