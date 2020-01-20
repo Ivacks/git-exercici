84921d71179c37648f47f141c79238e3ffbfdd8b
Ivacks <ivan.navarro.vargas@gmail.com>
2020-01-20 11:53:10 +0100

    Subida Fase 3 C#

---
+++ Ejercicio_letras/C#/Fase3/Fase3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fase3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> nom = new Dictionary<char, int>();
+
+            // Rellenar diccionario
+
+            nom.Add('I', 1);
+            nom.Add('V', 1);
+            nom.Add('A', 1);
+            nom.Add('N', 1);
+
+            foreach(KeyValuePair<char, int> lletra in nom)
+            {
+                Console.WriteLine("Lletra: " + lletra.Key + " numero de intentos: " +  lletra.Value);
+            }
+
+        }
+    }
+}
