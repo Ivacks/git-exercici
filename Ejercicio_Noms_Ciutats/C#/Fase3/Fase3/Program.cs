71fbf55772e1630c45b8b55704e70aba1ac42334
Ivacks <ivan.navarro.vargas@gmail.com>
2020-01-21 12:34:16 +0100

    Fase 3 C# Nom Ciutats

---
+++ Ejercicio_Noms_Ciutats/C#/Fase3/Fase3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fase3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ciutats = {"B4rcelon4", "M4drid", "V4lenci4", "M4l4l4", "C4dis", "S4nt4nder"};
+
+            Array.Sort(ciutats);
+
+            Console.WriteLine("Las ciudades introducidas son las siguientes: ");
+            for (int i = 0; i < ciutats.Length; i++)
+            {
+                Console.Write(ciutats[i] + " ");
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
