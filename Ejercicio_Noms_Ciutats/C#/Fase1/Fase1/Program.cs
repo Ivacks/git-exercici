2fbaea388b2bf84f7866173c9bcac2b6006d0612
Ivacks <ivan.navarro.vargas@gmail.com>
2020-01-21 11:19:31 +0100

    Subida fase 1 C# Nom Ciutats

---
+++ Ejercicio_Noms_Ciutats/C#/Fase1/Fase1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdueix el nom de la ciutat 1");
+            string ciutat1 = Console.ReadLine();
+            Console.WriteLine("Introdueix el nom de la ciutat 2");
+            string ciutat2 = Console.ReadLine();
+            Console.WriteLine("Introdueix el nom de la ciutat 3");
+            string ciutat3 = Console.ReadLine();
+            Console.WriteLine("Introdueix el nom de la ciutat 4");
+            string ciutat4 = Console.ReadLine();
+            Console.WriteLine("Introdueix el nom de la ciutat 5");
+            string ciutat5 = Console.ReadLine();
+            Console.WriteLine("Introdueix el nom de la ciutat 6");
+            string ciutat6 = Console.ReadLine();
+
+            Console.WriteLine($"Las ciudades introducidas son las siguientes: {ciutat1}, {ciutat2}, {ciutat3}, {ciutat4}, {ciutat5}, {ciutat6}.");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
