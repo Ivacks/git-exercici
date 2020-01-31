5630daba9d32490345088b53a9336aa49f1f7c89
Ivacks <ivan.navarro.vargas@gmail.com>
2020-01-31 09:51:19 +0100

    Subida Fase 1 LINQ

---
+++ Ejercicio_LINQ/Fase1/Fase1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Fase1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
+
+            var numbers_odd = numbers.Where(x => x % 2 != 1);
+
+            foreach(int odd in numbers_odd)
+            {
+                Console.WriteLine($"Este numero es par: {odd}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
