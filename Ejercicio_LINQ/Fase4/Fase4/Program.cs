538f79206b3d9168169796f3c493d2e2b8c3566d
Ivacks <ivan.navarro.vargas@gmail.com>
2020-01-31 12:06:04 +0100

    Subida Fase 4 LINQ

---
+++ Ejercicio_LINQ/Fase4/Fase4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Fase4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] noms = {"David", "Sergio", "Maria", "Laura", "Oscar", "Julia", "Oriol" };
+
+            var noms_O = from nom in noms
+                         where nom.Contains("O")
+                         orderby nom descending
+                         select nom;
+
+            var noms_lenght = from nom in noms
+                              where nom.Length >= 6
+                              orderby nom descending
+                              select nom;
+
+            var noms_sort = from nom in noms
+                            orderby nom descending
+                            select nom;
+
+
+            foreach (string ou in noms_O)
+            {
+                Console.WriteLine($"Aquest nom comença per O: {ou}.");
+            }
+
+            Console.WriteLine("****");
+
+            foreach (string lenght in noms_lenght)
+            {
+                Console.WriteLine($"Aquest nom supera els 6 caracteres: {lenght}.");
+            }
+
+            Console.WriteLine("****");
+
+            foreach (string descend in noms_sort)
+            {
+                Console.WriteLine($"{descend}");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
