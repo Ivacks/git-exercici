1f8829ba0efa3271028a654bbf5cc6c05aeb27f3
Ivacks <ivan.navarro.vargas@gmail.com>
2020-01-31 10:19:40 +0100

    Subida Fase 2 LINQ

---
+++ Ejercicio_LINQ/Fase2/Fase2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Fase2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = { 2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4 };
+
+            var max = numbers.Max();
+
+            var min = numbers.Min();
+
+            var avr = numbers.Average();
+
+            Console.WriteLine($"El número màxim és: {max}.");
+            Console.WriteLine($"El número mínim és: {min}." );
+            Console.WriteLine($"La mitjana és: {avr}.");
+
+
+
+
+        }
+    }
+}
